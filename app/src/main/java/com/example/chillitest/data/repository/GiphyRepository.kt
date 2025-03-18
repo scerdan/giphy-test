b55ebc63760a94138a2dff3225dd94ee5f4adb51
sCerdan <sjcerdansantiagojoel@teco.com.ar>
2025-03-18 09:09:35 -0300

    Set GiphyRepository

--- app/src/main/java/com/example/chillitest/data/repository/GiphyRepository.kt
+++ app/src/main/java/com/example/chillitest/data/repository/GiphyRepository.kt
@@ -1,11 +1,27 @@
 package com.example.chillitest.data.repository
 
 import com.example.chillitest.data.service.ApiService
+import com.example.chillitest.data.states.ResultTypes
+import com.example.chillitest.domain.models.DataResponse
+import retrofit2.HttpException
+import java.io.IOException
 import javax.inject.Inject
 
-class GiphyRepository @Inject constructor(private val client: ApiService) {
+class GiphyRepository @Inject constructor(private val apiService: ApiService) {
 
-    suspend fun getGiffs(query: String) {
-        client.randomGifs(apiKey = BuildConfig, q = query)
+    suspend fun startSearch(query: String, limit: Int = 25, offset: Int = 0): ResultTypes<DataResponse> {
+        return try {
+            val response = apiService.searchGifs(query, limit, offset)
+            when {
+                response.isSuccessful -> ResultTypes.Success(response.body())
+                else -> ResultTypes.HttpError(HttpException(response))
+            }
+        } catch (e: Throwable) {
+            when (e) {
+                is IOException -> ResultTypes.IOError(e)
+                is HttpException -> ResultTypes.HttpError(e)
+                else -> ResultTypes.Error(e)
+            }
+        }
     }
 }
