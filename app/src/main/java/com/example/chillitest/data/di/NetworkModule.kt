6eff72fb14f08cb961e1c56c8b9ae7cfdb1c6411
sCerdan <sjcerdansantiagojoel@teco.com.ar>
2025-03-18 19:11:19 -0300

    Set Icon

--- app/src/main/java/com/example/chillitest/data/di/NetworkModule.kt
+++ app/src/main/java/com/example/chillitest/data/di/NetworkModule.kt
@@ -16,7 +16,7 @@ import javax.inject.Singleton
 object NetworkModule {
 
     private const val BASE_URL = "https://api.giphy.com/v1/"
-    private const val API_KEY = "ZYyuP9xbYvtLvnJy85C4fyzKdGixE9Ah"
+    private const val API_KEY = "ZYyuP9xbYvtLvnJy85asdC4fyzKdGixE9Ah"
 
     @Provides
     @Singleton
