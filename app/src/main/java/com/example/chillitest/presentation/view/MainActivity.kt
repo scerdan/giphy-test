972cf1e188b9c1a4539653ba45e5b93ff46d1c97
sCerdan <sjcerdansantiagojoel@teco.com.ar>
2025-03-18 09:38:54 -0300

    Set Grid

--- app/src/main/java/com/example/chillitest/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/chillitest/presentation/view/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.chillitest.data.states.ResultTypes
 import com.example.chillitest.presentation.viewmodel.GiphyViewModel
 import com.example.chillitest.ui.theme.ChilliTestTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -32,14 +33,17 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun GiphyScreen(viewModel: GiphyViewModel = hiltViewModel()) {
-    val gifs by viewModel.gifState.collectAsState()
-    viewModel.searchGifs("CATS")
-
-
-    Text(
-        text = "$gifs!"
-    )
-}
+//
+//@Composable
+//fun GiphyScreen(viewModel: GiphyViewModel = hiltViewModel()) {
+//    val gifs by viewModel.gifState.collectAsState()
+//    viewModel.searchGifs("CATS")
+//
+//    when(gifs) {
+//        is ResultTypes.Error -> TODO()
+//        is ResultTypes.HttpError -> TODO()
+//        is ResultTypes.IOError -> TODO()
+//        is ResultTypes.Loading -> TODO()
+//        is ResultTypes.Success -> TODO()
+//    }
+//}
---
+++ app/src/main/java/com/example/chillitest/presentation/view/MainScreen.kt
@@ -0,0 +1,82 @@
+package com.example.chillitest.presentation.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import com.example.chillitest.data.states.ResultTypes
+import com.example.chillitest.presentation.viewmodel.GiphyViewModel
+
+@Composable
+fun GiphyScreen(viewModel: GiphyViewModel = hiltViewModel()) {
+    var searchQuery by remember { mutableStateOf("") }
+
+    val gifs by viewModel.gifState.collectAsState()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        TextField(
+            value = searchQuery,
+            onValueChange = { searchQuery = it },
+            label = { Text("Search GIFs") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    viewModel.searchGifs(searchQuery)
+                }
+            )
+        )
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            modifier = Modifier.fillMaxSize()
+        ) {
+            when (gifs) {
+                is ResultTypes.Success -> {
+                    val data = (gifs as ResultTypes.Success).data?.data
+
+                    if (data != null) {
+                        items(data.size) { gif ->
+                            AsyncImage(
+                                model = data[gif].url,
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .padding(4.dp)
+                                    .aspectRatio(1f)
+                            )
+                        }
+                    }
+                }
+
+                is ResultTypes.Error -> TODO()
+                is ResultTypes.HttpError -> TODO()
+                is ResultTypes.IOError -> TODO()
+                ResultTypes.Loading -> TODO()
+            }
+        }
+    }
+}
